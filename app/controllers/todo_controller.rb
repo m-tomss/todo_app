0ba0b34cbdcf17498f510695e06b34fc163313a5
Manuel Diaz <manuelto1999@gmail.com>
2018-11-18 20:01:31 +0000

    app update

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -25,7 +25,7 @@ def update
   t.description = params['description']
   t.pomodoro_estimate = params['pomodoro_estimate']
   t.save
-  redirect_to "/todo/show/#{t.id}"
+  redirect_to "/todo/show#{t.id}"
     end
 def destroy
   t = Todo.find_by_id(params[:id])
